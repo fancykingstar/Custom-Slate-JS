543ca29a727a903deda0c48771f06810010a167c
Han Lee <bonafidehan@gmail.com>
2020-07-03 10:53:18 -0700

    Slate hello world.

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react"
+import { createEditor, Node } from 'slate'
 
-function App() {
+import { Slate, Editable, withReact } from 'slate-react'
+
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+      <Editable />
+    </Slate>
+  )
 }
 
-export default App;
+export default App
