21274cd32e7c7e40bf4595c552242e762e8f7bb8
Han Lee <bonafidehan@gmail.com>
2020-07-22 13:01:06 -0700

    Removes options object.

--- prototype/app/src/Editor.tsx
+++ prototype/app/src/Editor.tsx
@@ -3,76 +3,41 @@ import { createEditor, Node } from 'slate'
 import { withHistory } from 'slate-history'
 import { Slate, withReact } from 'slate-react'
 import {
-  ParagraphPlugin,
+  DEFAULTS_BOLD,
+  DEFAULTS_PARAGRAPH,
+  DEFAULTS_ITALIC,
+  DEFAULTS_UNDERLINE,
   BoldPlugin,
   EditablePlugins,
+  HeadingPlugin,
   ItalicPlugin,
+  ParagraphPlugin,
   UnderlinePlugin,
+  SlateDocument,
   pipe,
 } from '@udecode/slate-plugins'
-import {
-  DEFAULTS_ALIGN,
-  DEFAULTS_BLOCKQUOTE,
-  DEFAULTS_BOLD,
-  DEFAULTS_CODE,
-  DEFAULTS_CODE_BLOCK,
-  DEFAULTS_HEADING,
-  DEFAULTS_HIGHLIGHT,
-  DEFAULTS_IMAGE,
-  DEFAULTS_ITALIC,
-  DEFAULTS_LINK,
-  DEFAULTS_LIST,
-  DEFAULTS_MEDIA_EMBED,
-  DEFAULTS_MENTION,
-  DEFAULTS_PARAGRAPH,
-  DEFAULTS_SEARCH_HIGHLIGHT,
-  DEFAULTS_STRIKETHROUGH,
-  DEFAULTS_SUBSUPSCRIPT,
-  DEFAULTS_TABLE,
-  DEFAULTS_TODO_LIST,
-  DEFAULTS_UNDERLINE,
-  SlateDocument,
-} from '@udecode/slate-plugins'
-
-const options = {
-  ...DEFAULTS_PARAGRAPH,
-  ...DEFAULTS_MENTION,
-  ...DEFAULTS_BLOCKQUOTE,
-  ...DEFAULTS_CODE_BLOCK,
-  ...DEFAULTS_LINK,
-  ...DEFAULTS_IMAGE,
-  ...DEFAULTS_MEDIA_EMBED,
-  ...DEFAULTS_TODO_LIST,
-  ...DEFAULTS_TABLE,
-  ...DEFAULTS_LIST,
-  ...DEFAULTS_HEADING,
-  ...DEFAULTS_ALIGN,
-  // marks
-  ...DEFAULTS_BOLD,
-  ...DEFAULTS_ITALIC,
-  ...DEFAULTS_UNDERLINE,
-  ...DEFAULTS_STRIKETHROUGH,
-  ...DEFAULTS_CODE,
-  ...DEFAULTS_SUBSUPSCRIPT,
-  ...DEFAULTS_HIGHLIGHT,
-  ...DEFAULTS_SEARCH_HIGHLIGHT,
-}
 
 const initialValue: Node[] = [
   {
-    type: options.p.type,
+    type: DEFAULTS_PARAGRAPH.p.type,
     children: [
       {
         text: 'This text is bold, italic and underlined.',
-        [options.bold.type]: true,
-        [options.italic.type]: true,
-        [options.underline.type]: true,
+        [DEFAULTS_BOLD.bold.type]: true,
+        [DEFAULTS_ITALIC.italic.type]: true,
+        [DEFAULTS_UNDERLINE.underline.type]: true,
       },
     ],
   }
 ]
 
-const plugins = [ParagraphPlugin(), BoldPlugin(), ItalicPlugin(), UnderlinePlugin()]
+const plugins = [
+  ParagraphPlugin(),
+  BoldPlugin(),
+  HeadingPlugin(),
+  ItalicPlugin(),
+  UnderlinePlugin()
+]
 
 const withPlugins = [withReact, withHistory] as const
 
