ce7f1133f1fb06e823ddfac5066825a75844ea1d
Han Lee <bonafidehan@gmail.com>
2020-07-22 21:02:17 -0700

    Replaces / with the words menu.

--- prototype/app/src/App.tsx
+++ prototype/app/src/App.tsx
@@ -1,7 +1,28 @@
-import { Editor } from './Editor'
+import React, { useMemo, useState } from 'react'
+import { Node, createEditor } from 'slate'
+import { Editable, Slate, withReact } from 'slate-react'
 
 const App = () => {
-  return Editor()
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+
+  return (
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable
+        onKeyDown={event => {
+          if (event.key === '/') {
+            event.preventDefault()
+            editor.insertText("menu")
+          }
+        }}
+      />
+    </Slate>
+  )
 }
 
 export default App
