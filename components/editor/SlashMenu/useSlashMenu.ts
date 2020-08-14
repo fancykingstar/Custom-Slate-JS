525caf1dd88e95d67ba44ba1d9fd5a233c507888
Andrew Lee <hi@andrewlee.design>
2020-08-13 17:52:58 -0700

    Very naive suggestion matching setup

--- components/editor/SlashMenu/useSlashMenu.ts
+++ components/editor/SlashMenu/useSlashMenu.ts
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from 'react';
 import { Range, Editor, Path, Transforms } from 'slate';
 import { ReactEditor } from 'slate-react';
 import fuzzy from 'fuzzy';
+import stemmer from 'stemmer';
 import insertCategorizerTool from 'components/elements/Categorizer/insertCategorizerTool';
 import insertChoicesTool from 'components/elements/Choices/insertChoicesTool';
 import insertGoalsTool from 'components/elements/Goals/insertGoalsTool';
@@ -86,6 +87,139 @@ export const slashMenuItems: MenuItem[] = [
     comingSoon: true,
   },
 ];
+
+interface Suggestion {
+  text: string;
+  keywords: string[];
+  tool?: MenuItemTitle;
+}
+
+interface StemMatch {
+  stem: string;
+  keywords: string[];
+}
+
+interface SuggestionMatch {
+  suggestion: Suggestion;
+  matches: StemMatch[];
+}
+
+interface SlashMenuContent {
+  suggestion: {
+    text: string;
+    item: MenuItem;
+  } | null;
+  items: MenuItem[];
+  isFiltered: boolean;
+}
+
+const suggestions: Suggestion[] = [
+  {
+    text: 'Check out the Choices tool! It can help you find new options.',
+    keywords: ['choic', 'option'],
+    tool: MenuItemTitle.Choices,
+  },
+];
+
+function getMenuContent(query: string): SlashMenuContent {
+  const stems = query.split(' ').map((word) => stemmer(word));
+
+  // Go through all available suggestions to look for matches
+  const suggestionMatches: SuggestionMatch[] = suggestions.reduce(
+    (matchList, suggestion) => {
+      const { keywords } = suggestion;
+
+      // Go through each stem to look for matching keywords
+      const stemMatches: StemMatch[] = stems.reduce((stemMatchList, stem) => {
+        const matchingKeywords = keywords.filter((keyword) => keyword === stem);
+        if (matchingKeywords.length) {
+          stemMatchList.push({
+            stem,
+            keywords: matchingKeywords,
+          });
+        }
+
+        return stemMatchList;
+      }, [] as StemMatch[]);
+
+      if (stemMatches.length) {
+        matchList.push({
+          suggestion,
+          matches: stemMatches,
+        });
+      }
+
+      return matchList;
+    },
+    [] as SuggestionMatch[]
+  );
+
+  // Find the suggestion with the most matching keywords
+  const suggestionMatchWithMostMatches = suggestionMatches.reduce(
+    (suggestionMatch: SuggestionMatch | null, suggestion) => {
+      if (suggestionMatch == null) {
+        return suggestion;
+      }
+
+      const prevKeywordCount = suggestionMatch.matches.reduce(
+        (count, match) => count + match.keywords.length,
+        0
+      );
+
+      const newKeywordCount = suggestion.matches.reduce(
+        (count, match) => count + match.keywords.length,
+        0
+      );
+
+      return newKeywordCount > prevKeywordCount ? suggestion : suggestionMatch;
+    },
+    null
+  );
+
+  const suggestion = suggestionMatchWithMostMatches?.suggestion ?? null;
+
+  const filteredItems = fuzzy
+    .filter(query, slashMenuItems, {
+      extract: (item) =>
+        [
+          item.title,
+          item.description,
+          item.category,
+          item.keywords.join(' '),
+        ].join(' '),
+    })
+    .map((item) => item.original)
+    .flat();
+
+  // Remove suggestion from filtered items
+  if (suggestion != null) {
+    const { tool } = suggestion;
+    if (tool != null) {
+      const matchIndex = filteredItems.findIndex((item) => item.title === tool);
+      if (matchIndex > -1) {
+        filteredItems.splice(matchIndex, 1);
+      }
+    }
+  }
+
+  const suggestionMenuItem = slashMenuItems.find(
+    (item) => item.title === suggestion?.tool
+  );
+
+  const isFiltered = filteredItems.length !== slashMenuItems.length;
+
+  return {
+    suggestion:
+      suggestion != null && suggestionMenuItem != null
+        ? {
+            text: suggestion.text,
+            item: suggestionMenuItem,
+          }
+        : null,
+    items: filteredItems,
+    isFiltered,
+  };
+}
 
 type onChangeFn = () => void;
 type onKeyDownFn = (event: KeyboardEvent) => void;
@@ -173,7 +307,7 @@ export default function useSlashMenu(
 
     // Get the current word closest to the start of the selection
     const wordStart = Editor.before(editor, startOfSelection, {
-      unit: 'word',
+      unit: 'line',
     });
 
     // Go back one offset (to try to capture the slash character)
@@ -193,13 +327,9 @@ export default function useSlashMenu(
       Editor.range(editor, wordStartWithOffset, startOfSelection);
 
     const beforeText = beforeRange && Editor.string(editor, beforeRange);
-    const beforeMatch = beforeText && beforeText.match(/^\/(\w*)$/);
 
-    // Ensure there's an empty space or nothing after the command
-    const after = Editor.after(editor, startOfSelection);
-    const afterRange = Editor.range(editor, startOfSelection, after);
-    const afterText = Editor.string(editor, afterRange);
-    const afterMatch = afterText.match(/^(\s|$)/);
+    // Look for empty slash OR slash with immediate character after
+    const beforeMatch = beforeText && beforeText.match(/^\/(\w.*)?$/);
 
     if (
       // Prevent slash menu after start of line
@@ -208,44 +338,16 @@ export default function useSlashMenu(
       startOfSelection.path.length === 2 &&
       caretLine > 0 &&
       beforeRange &&
-      beforeMatch &&
-      afterMatch
+      beforeMatch
     ) {
       setIndex(0);
 
       // Filter available items by the query
-      const query = beforeMatch[1];
-      const filteredItems = fuzzy
-        .filter(query, slashMenuItems, {
-          extract: (item) =>
-            [
-              item.title,
-              item.description,
-              item.category,
-              item.keywords.join(' '),
-            ].join(' '),
-        })
-        .map((item) => item.original)
-        // Group items by category — not the best user experience, but
-        // the first item is always the best match still
-        .reduce((storage: MenuItem[][], item) => {
-          const { category } = item;
+      const query = beforeMatch[1] ?? '';
 
-          const storageCategory = storage.find(
-            (storedCategory) => storedCategory[0].category === category
-          );
+      getMenuContent(query);
 
-          if (storageCategory == null) {
-            storage.push([item]);
-          } else {
-            storageCategory.push(item);
-          }
-
-          return storage;
-        }, [])
-        .flat();
-
-      setItems(filteredItems);
+      setItems(slashMenuItems);
 
       // Update the range to cause a recalculation of the menu position
       setRange(beforeRange);
