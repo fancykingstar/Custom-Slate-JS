18ad1523e229091c7264f845870f389a9c037700
Han Lee <bonafidehan@gmail.com>
2020-09-10 21:10:46 -0700

    Clock icon. (#163)

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -1,3 +1,23 @@
+export function Clock(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="feather feather-clock"
+      viewBox="0 0 24 24"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M12 6L12 12 16 14" />
+    </svg>
+  );
+}
+
 export function ChevronUp(): JSX.Element {
   return (
     <svg
