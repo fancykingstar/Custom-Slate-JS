0dec43acd0bce2a258b4bfd9fde4ed05bdb3be91
Han Lee <bonafidehan@gmail.com>
2020-09-21 00:05:48 -0700

    Generated text is highlighted. (#191)
    * Adds suggested star highlight for GPT-3 suggestions.
    
    * Naive distance measurement to use for highlighting.
    
    * Show zap symbol for generated text.

--- components/editor/Leaf.tsx
+++ components/editor/Leaf.tsx
@@ -1,7 +1,10 @@
 import { MouseEvent, useContext } from 'react';
 
+import levenshtein from 'fast-levenshtein';
 import { RenderLeafProps } from 'slate-react';
 
+import { Author } from 'components/editor/author';
+import { Zap } from 'components/icons/Icons';
 import { Action, Store } from 'store/store';
 
 import styles from './Leaf.module.scss';
@@ -11,14 +14,27 @@ export default function Leaf(props: RenderLeafProps): JSX.Element {
   const { dispatch } = useContext(Store);
 
   let className;
+  let magicSymbol;
   if (leaf.slashHightlight) {
     className = styles.slashHighlight;
   } else if (leaf.suggestedStar && leaf.text !== '') {
     className = styles.suggestedStar;
+  } else if (leaf.author && leaf.author === Author.Deca) {
+    const original = leaf.original as string;
+    const distance = levenshtein.get(leaf.text, original);
+    if (original.length && distance / original.length <= 0.5) {
+      magicSymbol = (
+        <span className={styles.zap} contentEditable={false}>
+          <Zap />
+        </span>
+      );
+      className = styles.generated;
+    }
   }
 
   return (
     <span {...attributes} className={className}>
+      {magicSymbol}
       {children}
     </span>
   );
