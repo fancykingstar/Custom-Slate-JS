1b7dc6f804092a9dceb557ca7776450ca1786e16
Han Lee <bonafidehan@gmail.com>
2020-10-27 15:05:17 -0700

    Converts Deca logo from text to svg. (#302)

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -3,8 +3,16 @@ import styles from './Logo.module.scss';
 export default function Logo(): JSX.Element {
   return (
     <div className={styles.logo}>
-      <span className={styles.symbol}>D</span>
-      <span className={styles.wordmark}>Deca</span>
+      <img
+        className={styles.symbol}
+        src="/img/logo/symbol.svg"
+        alt="Deca symbol"
+      />
+      <img
+        className={styles.wordmark}
+        src="/img/logo/wordmark.svg"
+        alt="Deca wordmark"
+      />
     </div>
   );
 }
