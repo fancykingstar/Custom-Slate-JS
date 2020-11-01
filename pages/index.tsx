ff1a04e0b8a8284755c106561aa275f86cc28d6c
Han Lee <bonafidehan@gmail.com>
2020-10-31 19:35:47 -0700

    Favicon. (#323)

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,6 +96,7 @@ export default function Home(env: Env): JSX.Element {
         <title>Deca</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/img/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
           rel="stylesheet"
