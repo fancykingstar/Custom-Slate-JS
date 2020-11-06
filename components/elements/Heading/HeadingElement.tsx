6ded4984a23ae48eed518579be15874638512f84
fancykingstar <fancykingstar2019@gmail.com>
2020-11-06 07:58:37 -0500

    revise heading element

--- components/elements/Heading/HeadingElement.tsx
+++ components/elements/Heading/HeadingElement.tsx
@@ -14,6 +14,25 @@ export default function HeadingElement(
   const selected = useSelected();
   const { type } = element;
 
+  const getHeading = () => {
+    if (type === 'h1') {
+      return <h1 {...attributes}>{children}</h1>;
+    }
+    if (type === 'h2') {
+      return <h2 {...attributes}>{children}</h2>;
+    }
+    if (type === 'h3') {
+      return <h3 {...attributes}>{children}</h3>;
+    }
+    if (type === 'h4') {
+      return <h4 {...attributes}>{children}</h4>;
+    }
+    if (type === 'h5') {
+      return <h5 {...attributes}>{children}</h5>;
+    }
+    return <h6 {...attributes}>{children}</h6>;
+  };
+
   return (
     <div className={styles.headingElementContainer}>
       {dragHandleProps && (
@@ -26,7 +45,7 @@ export default function HeadingElement(
           <Drag />
         </div>
       )}
-      <h1 {...attributes}>{children}</h1>
+      {getHeading()}
     </div>
   );
 }
